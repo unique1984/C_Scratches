04a80c76d8c2b21277cca12b4de3234b2b4a8d65
Yasin KARABULAK <yasinkarabulak@gmail.com>
2018-08-08 01:56:25 +0300

    hex2rgb added

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # C_Scratches
-Better than nothing, templates from me.
+
+*Better than nothing, templates from me.*
+-----
+
+**08.08.2018** hex2rgb.c
+
+* **Building**
+	* gcc hex2rgb.c -o hex2rgb
+* **Using**
+	* ./hex2rgb FF00FF
+```
+yasin@uxn:~/C_Education/test$ ./hexTorgb FFFFFF
+Hex24   #FFFFFF
+RGB888 (255, 255, 255)
+
+Hex16   #FFFF
+RGB565 (63488, 2016, 31)
+```
+	*or*
+```
+yasin@uxn:~/C_Education/test$ ./hexTorgb 
+Enter 24bit hexValue: FF00FF
+Hex24   #FF00FF
+RGB888 (255, 0, 255)
+
+Hex16   #F81F
+RGB565 (63488, 0, 31)
+```
---
+++ hex2rgb.c
@@ -0,0 +1,61 @@
+/*****************************************
+ * App		: hex color to rgb values RGB888 & RGB565
+ * File		: hex2rgb.c
+ * Date		: Wed Aug  8 01:46:01 +03 2018
+ * Version	: 1.0.0
+ * Email	: yasinkarabulak@gmail.com
+ * 
+ * MIT License
+ * Copyright (c) 2018 Yasin KARABULAK
+*******************************************/
+#include <stdio.h>
+typedef struct hex2rgb
+{
+	unsigned int hexValue;
+	unsigned short r, g, b;
+} hex2rgb;
+
+hex2rgb parseRgb888( hex2rgb  hexValue );
+hex2rgb parseRgb565( hex2rgb  hexValue );
+
+int main( int argc, char *argv[] )
+{
+	hex2rgb hex24, hexParsed565, hexParsed888;
+	if ( argc > 1 )
+	{
+		sscanf( argv[1] , "%06x", &hex24.hexValue );
+	}
+	else
+	{
+		printf("Enter 24bit hexValue: ");
+		scanf("%06x", &hex24.hexValue);
+	}
+	hexParsed888 = parseRgb888( hex24 );
+	hexParsed565 = parseRgb565( hex24 );
+	printf ( "Hex24\t#%06X\nRGB888 (%d, %d, %d)\n\n", hex24.hexValue ,hexParsed888.r, hexParsed888.g, hexParsed888.b );
+	printf ( "Hex16\t#%04X\nRGB565 (%d, %d, %d)\n", hexParsed565.hexValue, hexParsed565.r, hexParsed565.g, hexParsed565.b );
+	return 0;
+}
+
+hex2rgb parseRgb565 ( hex2rgb hexParse )
+{
+	hex2rgb hexParsed888, parsed;
+	hexParsed888 = parseRgb888( hexParse );
+	parsed.r = ( ( hexParsed888.r >> 3 ) << 11 );
+	parsed.g = ( ( hexParsed888.g >> 2 ) << 5 );
+	parsed.b = ( hexParsed888.b >> 3 );
+	parsed.hexValue = ( parsed.r | parsed.g | parsed.b );
+	//printf( "Output\nHex 565: #%04X\n", parsed.hexValue );
+return parsed;
+}
+
+
+hex2rgb parseRgb888( hex2rgb hexParse ){
+	hex2rgb parsed;
+	parsed.r = ( ( hexParse.hexValue >> 16 ) & 0xff );
+	parsed.g = ( ( hexParse.hexValue >> 8 ) & 0xff );
+	parsed.b = ( ( hexParse.hexValue >> 0 ) & 0xff );
+	//printf( "Output\nHex 888: #%06X\n", hexParse.hexValue );
+	//printf( "RGB (%d, %d, %d)\n", parsed.r, parsed.g, parsed.b );
+return parsed;
+}
